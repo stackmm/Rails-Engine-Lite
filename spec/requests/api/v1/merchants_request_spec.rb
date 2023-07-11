81cfa1c05c2c1225d0ce06980559969666af3954
Steven Stackhouse <stackmm@gmail.com>
2023-07-10 18:49:32 -0600

    setup all merchants test and add serializer to controller

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,11 +2,12 @@ require "rails_helper"
 
 describe "Merchants API" do
   it "sends a list of merchants" do
-    # FactoryBot.create_list(:merchant, 3)
+    create_list(:merchant, 3)
 
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+    expect(response.status).to eq(200)
 
     merchants = JSON.parse(response.body, symbolize_names: true)
   end
