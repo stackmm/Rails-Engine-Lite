8719cc3d88437df212f294b787dc78930b63608a
Steven Stackhouse <stackmm@gmail.com>
2023-07-11 17:58:31 -0600

    add update item functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: "merchants/items"
       end
-      resources :items, only: [:index, :show, :create]
+      resources :items, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -92,6 +92,16 @@ RSpec.describe "Items API" do
   end
 
   it "can update an existing item" do
-    
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: "Silver Potatoe" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Silver Potatoe")
   end
 end
