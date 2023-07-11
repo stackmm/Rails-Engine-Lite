8139cdbb07a8a923d87d53fed9bd85a3d5198a71
Steven Stackhouse <stackmm@gmail.com>
2023-07-10 20:07:55 -0600

    add merchant show tests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -27,4 +27,25 @@ describe "Merchants API" do
       expect(merchant[:attributes][:name]).to be_an(String)
     end
   end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(String)
+
+    expect(merchant).to have_key(:type)
+    expect(merchant[:type]).to be_an(String)
+
+    expect(merchant).to have_key(:attributes)
+    expect(merchant[:attributes]).to be_an(Hash)
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_an(String)
+  end
 end
