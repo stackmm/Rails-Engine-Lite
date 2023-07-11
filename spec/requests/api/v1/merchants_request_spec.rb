c16b8b9b18b3c15a7ec302d5ac154ff383cbb24a
Steven Stackhouse <stackmm@gmail.com>
2023-07-10 19:52:16 -0600

    finish adding serializer to all merchants tests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,5 +10,21 @@ describe "Merchants API" do
     expect(response.status).to eq(200)
 
     merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data]).to be_an(Array)
+    expect(merchants[:data].count).to eq(3)
+
+    merchants[:data].each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(String)
+
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_an(String)
+
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to be_an(Hash)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_an(String)
+    end
   end
 end
