75f36d8f0a6be60189ac7381652b3f7d4f8ba088
Steven Stackhouse <stackmm@gmail.com>
2023-07-11 17:46:54 -0600

    change routes to be more RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index]
+        resources :items, only: [:index], controller: "merchants/items"
       end
       resources :items, only: [:index, :show, :create]
     end
