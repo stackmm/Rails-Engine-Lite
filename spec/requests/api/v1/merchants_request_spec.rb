01b9e7902b135029ae8c2fa5ac25173487c252a5
Steven Stackhouse <stackmm@gmail.com>
2023-07-12 11:25:12 -0600

    refactor merchant spec file

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,97 +1,103 @@
 require "rails_helper"
 
-describe "Merchants API" do
-  it "sends a list of merchants" do
-    create_list(:merchant, 3)
-
-    get "/api/v1/merchants"
-
-    expect(response).to be_successful
-
-    merchants = JSON.parse(response.body, symbolize_names: true)
-
-    expect(merchants[:data]).to be_an(Array)
-    expect(merchants[:data].count).to eq(3)
-
-    merchants[:data].each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(String)
-
-      expect(merchant).to have_key(:type)
-      expect(merchant[:type]).to be_an(String)
-
-      expect(merchant).to have_key(:attributes)
-      expect(merchant[:attributes]).to be_an(Hash)
-      expect(merchant[:attributes]).to have_key(:name)
-      expect(merchant[:attributes][:name]).to be_an(String)
+describe "Merchants API", type: :request do
+  describe "/api/v1/merchants" do
+    it "sends a list of merchants" do
+      create_list(:merchant, 3)
+  
+      get "/api/v1/merchants"
+  
+      expect(response).to be_successful
+  
+      merchants = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(merchants[:data]).to be_an(Array)
+      expect(merchants[:data].count).to eq(3)
+  
+      merchants[:data].each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_an(String)
+  
+        expect(merchant).to have_key(:type)
+        expect(merchant[:type]).to be_an(String)
+  
+        expect(merchant).to have_key(:attributes)
+        expect(merchant[:attributes]).to be_an(Hash)
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_an(String)
+      end
     end
   end
 
-  it "can get one merchant by its id" do
-    id = create(:merchant).id
-
-    get "/api/v1/merchants/#{id}"
-
-    merchant = JSON.parse(response.body, symbolize_names: true)
-
-    expect(response).to be_successful
-
-    expect(merchant[:data]).to have_key(:id)
-    expect(merchant[:data][:id]).to be_an(String)
-
-    expect(merchant[:data]).to have_key(:type)
-    expect(merchant[:data][:type]).to be_an(String)
-
-    expect(merchant[:data]).to have_key(:attributes)
-    expect(merchant[:data][:attributes]).to be_an(Hash)
-    expect(merchant[:data][:attributes]).to have_key(:name)
-    expect(merchant[:data][:attributes][:name]).to be_an(String)
-  end
-
-  it "can get all items for a merchant" do
-    merchant = create(:merchant)
-    create_list(:item, 3, merchant_id: merchant.id)
-
-    get "/api/v1/merchants/#{merchant.id}/items"
-
-    items = JSON.parse(response.body, symbolize_names: true)
-
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
-    
-    expect(items[:data]).to be_an(Array)
-    expect(items[:data].count).to eq(3)
-
-    items[:data].each do |item|
-      expect(item).to have_key(:id)
-      expect(item[:id]).to be_an(String)
-
-      expect(item).to have_key(:type)
-      expect(item[:type]).to be_an(String)
-
-      expect(item).to have_key(:attributes)
-      expect(item[:attributes]).to be_an(Hash)
-      expect(item[:attributes]).to have_key(:name)
-      expect(item[:attributes][:name]).to be_an(String)
-
-      expect(item[:attributes]).to have_key(:description)
-      expect(item[:attributes][:description]).to be_an(String)
-
-      expect(item[:attributes]).to have_key(:unit_price)
-      expect(item[:attributes][:unit_price]).to be_an(Float)
-
-      expect(item[:attributes]).to have_key(:merchant_id)
-      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+  describe "/api/v1/merchants/:id" do
+    it "can get one merchant by its id" do
+      id = create(:merchant).id
+  
+      get "/api/v1/merchants/#{id}"
+  
+      merchant = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to be_successful
+  
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data][:id]).to be_an(String)
+  
+      expect(merchant[:data]).to have_key(:type)
+      expect(merchant[:data][:type]).to be_an(String)
+  
+      expect(merchant[:data]).to have_key(:attributes)
+      expect(merchant[:data][:attributes]).to be_an(Hash)
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:attributes][:name]).to be_an(String)
     end
   end
 
-  it "rejects a request for items if merchant does not exist" do
-    get "/api/v1/merchants/1/items"
+  describe "/api/v1/merchants/merchant_id/items" do
+    it "can get all items for a merchant" do
+      merchant = create(:merchant)
+      create_list(:item, 3, merchant_id: merchant.id)
+  
+      get "/api/v1/merchants/#{merchant.id}/items"
+  
+      items = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      
+      expect(items[:data]).to be_an(Array)
+      expect(items[:data].count).to eq(3)
+  
+      items[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_an(String)
+  
+        expect(item).to have_key(:type)
+        expect(item[:type]).to be_an(String)
+  
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes]).to be_an(Hash)
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_an(String)
+  
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_an(String)
+  
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_an(Float)
+  
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      end
+    end
 
-    items = JSON.parse(response.body, symbolize_names: true)
-
-    expect(response).to_not be_successful
-    expect(response.status).to eq(404)
-    expect(items[:error]).to eq("Merchant not found")
+    it "rejects a request for a merchant's items if the merchant does not exist" do
+      get "/api/v1/merchants/1/items"
+  
+      items = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      expect(items[:error]).to eq("Merchant not found")
+    end
   end
 end
