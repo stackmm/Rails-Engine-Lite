66037e927658330b2620593172773aaa972add5a
Steven Stackhouse <stackmm@gmail.com>
2023-07-12 14:29:24 -0600

    refactor show method

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,9 +1,6 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
-    name = params[:name].downcase
-    merchant = Merchant.find_by_name(name)
-    # merchant = Merchant.where("lower(name) LIKE ?", "%#{name}%").first
-    require 'pry'; binding.pry
+    merchant = Merchant.find_by_name(params[:name])
     render json: MerchantSerializer.new(merchant)
   end
 end
