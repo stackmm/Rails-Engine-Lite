2e88d4e3877b4c909cd03710975807bdbb9f1b67
Steven Stackhouse <stackmm@gmail.com>
2023-07-12 14:27:19 -0600

    add find_by_name class method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,10 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoice_items, through: :invoices
   has_many :customers, through: :invoices
+
+  def self.find_by_name(name)
+    where("lower(name) LIKE ?", "%#{name.downcase}%")
+      .order(:name)
+      .first
+  end
 end
