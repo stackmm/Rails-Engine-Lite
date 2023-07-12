82415b7580c52499a9b8c8d187e8c4406c4f2447
Steven Stackhouse <stackmm@gmail.com>
2023-07-12 15:10:53 -0600

    minor refactor

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,6 +1,19 @@
 class Api::V1::Merchants::SearchController < ApplicationController
+  before_action :verify_params
+
   def show
     merchant = Merchant.find_by_name(params[:name])
     render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+  def verify_params
+    if params[:name].nil?
+      render json: {error: "No search term provided"}, status: :bad_request
+    elsif Merchant.find_by_name(params[:name]).nil?
+      render json: {error: "Merchant not found"}, status: 404
+    else
+      return
+    end
   end
 end
