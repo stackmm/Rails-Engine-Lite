d9c11dd23d6e536e0767b24f398f3ad6246624c5
Steven Stackhouse <stackmm@gmail.com>
2023-07-11 18:17:23 -0600

    add status code 201

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,11 +8,15 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-    render json: ItemSerializer.new(Item.create(item_params))
+    render json: ItemSerializer.new(Item.create(item_params)), status: 201
   end
 
   def update
     render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
+  def destroy
+    render json: ItemSerializer.new(Item.destroy(params[:id]))
   end
 
   private
