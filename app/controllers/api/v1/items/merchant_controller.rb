0f5c0820ea6dd68e2a0aa83c00b9dfdbd71d7c4b
Steven Stackhouse <stackmm@gmail.com>
2023-07-12 00:02:15 -0600

    add verify_item check for an items merchant requests

--- app/controllers/api/v1/items/merchant_controller.rb
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::Items::MerchantController < ApplicationController
+  before_action :verify_item
+  
   def index
     item = Item.find(params[:item_id])
     merchant = Merchant.find(item.merchant_id)
     render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+  def verify_item
+    render json: {error: "Item not found"}, status: 404 unless Item.exists?(params[:item_id])
   end
 end
