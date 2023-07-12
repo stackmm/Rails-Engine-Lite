19196f217bcfa41527d0b846d58fdd929f0f5a23
Steven Stackhouse <stackmm@gmail.com>
2023-07-12 13:37:03 -0600

    add numericality validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,5 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   validates_presence_of :name, :description, :unit_price, :merchant_id
+  validates_numericality_of :unit_price, greater_than: 0
 end
