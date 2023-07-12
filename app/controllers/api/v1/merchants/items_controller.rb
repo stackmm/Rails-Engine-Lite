8132f8453aacf75a517ff1096f206ffb4e22b322
Steven Stackhouse <stackmm@gmail.com>
2023-07-11 22:20:30 -0600

    add check for no merchant

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
+  before_action :verify_merchant
+
   def index
     items = Item.where(merchant_id: params[:merchant_id])
     render json: ItemSerializer.new(items)
+  end
+
+  private
+  def verify_merchant
+    render json: {error: "Merchant not found"}, status: 404 unless Merchant.exists?(params[:merchant_id])
   end
 end
