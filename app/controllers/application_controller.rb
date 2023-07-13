f6d5521eb2c5502441d136e46a4d985218c3e293
Steven Stackhouse <stackmm@gmail.com>
2023-07-13 12:24:06 -0600

    add rescue function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :error_response
+
+  def error_response(error)
+    render json: ErrorSerializer.new(error).serialized_json, status: 404
+  end
 end
