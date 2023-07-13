105bf782fba79cd3ed234da0ebd286b6b8bf4f00
Steven Stackhouse <stackmm@gmail.com>
2023-07-13 15:55:39 -0600

    add invalid record method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound, with: :error_response
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found # no merchant/item with that id in database
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid # params incomplete for create/update
 
-  def error_response(error)
+  def record_not_found(error)
     render json: ErrorSerializer.new(error).serialized_json, status: 404
+  end
+
+  def record_invalid(error)
+    render json: ErrorSerializer.new(error).invalid_serialized_json, status: 400
   end
 end
