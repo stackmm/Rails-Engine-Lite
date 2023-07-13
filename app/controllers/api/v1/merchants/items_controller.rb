51134facacc3addb162670c4e4f665d22cd71510
Steven Stackhouse <stackmm@gmail.com>
2023-07-13 12:23:54 -0600

    refactor with error serialier

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
-  before_action :verify_merchant
-
   def index
+    Merchant.find(params[:merchant_id])
     items = Item.where(merchant_id: params[:merchant_id])
     render json: ItemSerializer.new(items)
-  end
-
-  private
-  def verify_merchant
-    render json: {error: "Merchant not found"}, status: 404 unless Merchant.exists?(params[:merchant_id])
   end
 end
