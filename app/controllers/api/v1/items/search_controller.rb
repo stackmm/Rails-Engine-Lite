1b791a0ffe06bb6243ec8adccf2d5a48dac1c797
Steven Stackhouse <stackmm@gmail.com>
2023-07-12 22:38:04 -0600

    add both max and min price range

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,13 +1,12 @@
 class Api::V1::Items::SearchController < ApplicationController
   def index
-
     # if params[:name] && (params[:min_price] || params[:max_price])
     if params[:name]
       items = Item.find_all_by_name(params[:name])
       render json: ItemSerializer.new(items)
-    # elsif params[:min_price] && params[:max_price]
-    #   items = Item.find_all_by_price_range(params[:min_price], params[:max_price])
-    #   render json: ItemSerializer.new(items)
+    elsif params[:min_price] && params[:max_price]
+      items = Item.find_all_by_price_range([params[:min_price], params[:max_price]])
+      render json: ItemSerializer.new(items)
     elsif params[:min_price]
       items = Item.find_all_by_min_price(params[:min_price])
       render json: ItemSerializer.new(items)
